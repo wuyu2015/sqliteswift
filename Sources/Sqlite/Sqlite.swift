45a1c9371266bf33e7cd8eb8db81736a9ed660df
wuyu <wuyu.code@gmail.com>
2024-04-30 07:04:58 +0800

    Add: init(path: String, flags: Int32)

--- Sources/Sqlite/Sqlite.swift
+++ Sources/Sqlite/Sqlite.swift
@@ -93,5 +93,37 @@ class Sqlite {
     static var isThreadSafe: Bool {
         return sqlite3_threadsafe() > 0
     }
+    
+    let db: OpaquePointer
+    
+    /*
+     flags:
+         SQLITE_OPEN_READONLY: 打开数据库为只读模式，不允许进行写操作。
+         SQLITE_OPEN_READWRITE: 打开数据库为读写模式，允许进行读和写操作。
+         SQLITE_OPEN_CREATE: 如果数据库不存在，则创建一个新的数据库。
+         SQLITE_OPEN_DELETEONCLOSE: 数据库在关闭时应该被删除。
+         SQLITE_OPEN_EXCLUSIVE: 数据库连接是独占的，其他连接不能访问。
+         SQLITE_OPEN_AUTOPROXY: 数据库连接应该尝试使用适合当前操作系统和环境的默认代理。
+         SQLITE_OPEN_URI: 标志允许数据库名是一个 URI，如文件路径。
+         SQLITE_OPEN_MEMORY: 打开一个内存数据库。
+         SQLITE_OPEN_MAIN_DB, SQLITE_OPEN_TEMP_DB, SQLITE_OPEN_TRANSIENT_DB: 这些标志是给虚拟文件系统 (VFS) 使用的，用于打开主数据库、临时数据库和临时的数据库。
+         SQLITE_OPEN_MAIN_JOURNAL, SQLITE_OPEN_TEMP_JOURNAL, SQLITE_OPEN_SUBJOURNAL, SQLITE_OPEN_MASTER_JOURNAL: 这些标志是给虚拟文件系统 (VFS) 使用的，用于打开主日志、临时日志、子日志和主日志。
+         SQLITE_OPEN_NOMUTEX: 禁用数据库连接的互斥锁。如果你确定只会在单个线程中使用 SQLite，可以禁用 SQLite 的互斥锁，以提高性能。
+         SQLITE_OPEN_FULLMUTEX: 启用数据库连接的全互斥锁。
+         SQLITE_OPEN_SHAREDCACHE: 启用数据库连接的共享缓存。
+         SQLITE_OPEN_PRIVATECACHE: 启用数据库连接的私有缓存。
+         SQLITE_OPEN_WAL: 启用数据库连接的写入日志模式。
+         SQLITE_OPEN_FILEPROTECTION_COMPLETE, SQLITE_OPEN_FILEPROTECTION_COMPLETEUNLESSOPEN, SQLITE_OPEN_FILEPROTECTION_COMPLETEUNTILFIRSTUSERAUTHENTICATION, SQLITE_OPEN_FILEPROTECTION_NONE, SQLITE_OPEN_FILEPROTECTION_MASK: 这些标志是用于设置数据库文件保护级别的，仅适用于 iOS 平台。
+     */
+    init(path: String = ":memory:", flags: Int32 = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE) {
+        var dbPointer: OpaquePointer?
+        guard sqlite3_open_v2(path, &dbPointer, flags, nil) == SQLITE_OK else {
+            fatalError("Failed to open database.")
+        }
+        db = dbPointer!
+    }
 
+    deinit {
+        sqlite3_close(db)
+    }
 }
